7737ca38ee67141eb8eb490c2920e8c433bf227b
Glenn Pierce <glenn.pierce@carnegosystems.com>
2016-08-12 12:33:12 +0100

    Rearange structure

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ from distutils.core import setup
 
 from distutils.core import setup, Extension
 
-/usr/lib/python2.7/site-packages/beetsplug/
-
 setup(name='PartyZone',
       version='1.0',
       description='Syncronise music across machines',
       author='Glenn Pierce',
       author_email='glennpierce@gmail.com',
       url='https://github.com/glennpierce/PartyZone',
-      packages = ['PartyZone'],
+      packages = ['', 'beetsplug'],
+      #install_requires=['beets>=1.2.2,==dev'],
+      scripts=["partyzone"],
      )
